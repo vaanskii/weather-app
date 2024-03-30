54b5ddc035dd49bece6f0b195e397fef1e485520
vaanskii <giorgivanadze03@gmail.com>
2024-03-30 08:32:19 +0400

    Integrate search on enter function in js.

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ async function checkWeather(city) {
     document.querySelector(".humidity").innerHTML = data.main.humidity + ' %';
     document.querySelector(".wind").innerHTML = data.wind.speed + ' km/h';
 
-    if(data.weather[0].main == "Clouds") {
+    if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "images/clouds.png";
-    }else if(data.weather[0].main == 'Clear') {
+    } else if (data.weather[0].main == 'Clear') {
         weatherIcon.src = "images/clear.png";
-    }else if(data.weather[0].main == 'Rain'){
+    } else if (data.weather[0].main == 'Rain') {
         weatherIcon.src = "images/rain.png";
-    }else if(data.weather[0].main == 'Drizzle') {
+    } else if (data.weather[0].main == 'Drizzle') {
         weatherIcon.src = 'images/drizzle.png'
-    }else if(data.weather[0].main == 'Mist') {
+    } else if (data.weather[0].main == 'Mist') {
         weatherIcon.src = "images/mist.png"
     };
 
@@ -33,3 +33,8 @@ searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
 
+searchBox.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
